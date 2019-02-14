a780a0f2b4e485eca41c47ae55d22bade081c576
David Goligorsky <d.goligorsky@gmail.com>
2019-02-13 20:23:23 -0500

    finished the stamps and added sound effect

--- js/stamps.js
+++ js/stamps.js
@@ -10,7 +10,30 @@ const stamps = [
 const stampsTag = document.querySelector("div.stamps")
 
 const addStamp = function(x, y) {
-    console.log(x, y)
+    // console.log(x, y)
+
+    const img = document.createElement("img")
+
+    img.setAttribute("src", stamps[number])
+
+    // remove half the window width to center the stamps
+    // this helps keep stamps in the right place on a window resize
+    img.style.left = (x - window.innerWidth / 2) + "px"
+    img.style.top = y + "px"
+
+    stampsTag.appendChild(img)
+
+    // add some audio
+
+    const audio = document.createElement("audio")
+    audio.setAttribute("src", "assets/plop.mp3")
+    audio.play()
+
+    number += 1
+
+    if (number >= stamps.length) {
+        number = 0
+    }
 }
 
 document.addEventListener("click", function(event) {
